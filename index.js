42bc6eaa47f42e898261ea02bd28db3256dcfb25
Tourki-hub <tourkimufarej@gmail.com>
2024-04-28 20:20:22 +0300

    Task 2 solve

--- index.js
+++ index.js
@@ -7,5 +7,19 @@ function calculateBMI() {
   let female = document.getElementById("f").checked;
   console.table({ age, weight, height, male, female });
 
-  // You are going to write the code here before the curly braces 👇🏻
+  // You are going to write the code here before the curly braces 
+  
+  let newHeight = height/100
+  let bmi = weight / newHeight**2;
+  alert(weight / newHeight**2)
+
+  if ( bmi < 18.5){
+    alert("Underweight");
+  }else if ( bmi >= 18.5 && bmi <= 24.9 ){
+    alert("Healthy Weight")
+  }else if (25.0 <= bmi  && bmi <= 29.9){
+    alert("Overweight")
+  } else{
+    alert("Don't Worry your in a good shape")
+  }
 }
